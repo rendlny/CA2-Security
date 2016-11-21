91719229c1bd6a4873d025bd24373cd7da3df1d0
Ren <Ren@10.102.22.232>
2016-11-21 21:06:04 +0000

    check

--- src/java/Servlet/Controller.java
+++ src/java/Servlet/Controller.java
@@ -32,17 +32,7 @@ public class Controller extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controller</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controller at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
             
             String forwardToJsp = "";
             String action = request.getParameter("action");
@@ -60,7 +50,7 @@ public class Controller extends HttpServlet {
             }else{
                 
             }
-        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">

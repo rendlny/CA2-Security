d07d849c9c4207b801a3bf8a4bb2b93f9837485e
Connor Pakenham <ConnorPakenham@gmail.com>
2016-12-01 10:51:36 +0000

    Added UserUpdatePasswordCommand File

---
+++ src/java/Commands/UserUpdatePasswordCommand.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Commands;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Conno
+ */
+public class UserUpdatePasswordCommand implements Command {
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
